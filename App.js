66fe456f192b6af0e665124e8817e8403c6ad879
Birlas <birlas@Birlass-MacBook-Pro.local>
2026-05-06 03:45:03 -0500

    touchable - wip

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
 import {useVideoPlayer, VideoView} from 'expo-video'
 import { useEvent } from 'expo';
 import React from 'react';
@@ -52,6 +52,11 @@ class CatVideoButton extends React.Component {
   
  render(){
   return(
+    <View>
+      <TouchableHighlight onPress={() => {
+        console.log("Pressed the cat.")
+      }}>
+      <View>
     <VideoView 
       player={this.props.player}
       
@@ -64,9 +69,15 @@ class CatVideoButton extends React.Component {
          allowsPictureInPicture
          contentFit="cover"
       />
+       
+    </View>
+    </TouchableHighlight>
+    </View>
         )
   }
+
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
